792bb891326210f8d55c4891a45c44567ba2e0a1
Antoine-CJ-Thomas <antoine.cj.thomas@gmail.com>
2021-04-26 18:01:21 +0200

    Added Swagger

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/Application.java
@@ -3,9 +3,12 @@ package com.nnk.springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * This class allows to start the application
  */
+@EnableSwagger2
 @SpringBootApplication
 public class Application {
 
